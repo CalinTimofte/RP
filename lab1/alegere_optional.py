8c01d9bf375394ad4bbab0412a2a192d2986a89d
platicaalex <platicaalex@gmail.com>
2020-03-03 10:47:23 +0200

    Commit message

--- lab1/alegere_optional.py
+++ lab1/alegere_optional.py
@@ -4,41 +4,43 @@ pachet6 = ["Psihologia comunicării profesionale în domeniul IT-lui", "Cloud Co
            "Tehnici de ingineria limbajului natural", "Analiza reţelelor media sociale"]
 pachet7 = ["Reţele Petri şi aplicaţii", "Smart Card-uri şi Aplicaţii", "Topici speciale de programare .NET"]
 
+raspunsuri_pachet6 = [0, 0, 0, 0]
+raspunsuri_pachet7 = [0, 0, 0]
 
-def alegere_optional_din_pachet(pachet):
-    intrare_user = None
-    for i, j in enumerate(pachet):
-        print(i, j)
-    while intrare_user not in range(len(pachet)):
-        print("Introduceti: ", end="")
-        for elem in range(len(pachet) - 1):
-            print(str(elem) + " ", end="")
-        print("sau " + str(len(pachet)-1))
-        try:
-            intrare_user = int(input())
-        except:
-            print("Input incorect")
-    return intrare_user
+intrebari_pachet_6 = ["Esti o persoana care are un interest pentru psihologie?",
+                      "Iti plac aplicatiile WEB?",
+                      "Te-a interesat Inteligenta Artificiala?",
+                      "Iti place sa urmaresti trendurile si vrei sa stii ce se ascunde in spatele retelelor media?"]
+
+intrebari_pachet_7 = ["Este interesat de Retele Petri?",
+                      "Vrei sa stii cum functioneaza cardul pe care iti tii banii?",
+                      "Vrei sa inveti .NET?"]
 
 
-def alegere_optional():
-    opt1 = None
-    opt2 = None
-    opt3 = None
-
-    print("Alegeti o optiune din pachetul 5: ")
-    opt1 = pachet5[alegere_optional_din_pachet(pachet5)]
-
-    print("Alegeti o optiune din pachetul 6: ")
-    opt2 = pachet6[alegere_optional_din_pachet(pachet6)]
-
-    print("Alegeti o optiune din pachetul 7: ")
-    opt3 = pachet7[alegere_optional_din_pachet(pachet7)]
-
-    print("Cursurile dumneavoastra sunt:")
-    print(opt1)
-    print(opt2)
-    print(opt3)
+def alegere_optional6():
+    print("Alegere optional pachet 6")
+    print("Variante de raspuns: 0 - Da; 1 - Nu; 2 - Nu stiu")
+    for intrebare in intrebari_pachet_6:
+        print(intrebare)
+        raspuns = int(input())
+        if raspuns == 0:
+            raspunsuri_pachet6[intrebari_pachet_6.index(intrebare)] += 2
+        if raspuns == 2:
+            raspunsuri_pachet6[intrebari_pachet_6.index(intrebare)] += 1
+    return pachet6[raspunsuri_pachet6.index(max(raspunsuri_pachet6))]
 
 
-alegere_optional()
+def alegere_optional7():
+    print("Alegere optional pachet 7")
+    print("Variante de raspuns: 0 - Da; 1 - Nu; 2 - Nu stiu")
+    for intrebare in intrebari_pachet_7:
+        print(intrebare)
+        raspuns = int(input())
+        if raspuns == 0:
+            raspunsuri_pachet7[intrebari_pachet_7.index(intrebare)] += 2
+        if raspuns == 2:
+            raspunsuri_pachet7[intrebari_pachet_7.index(intrebare)] += 1
+    return pachet7[raspunsuri_pachet7.index(max(raspunsuri_pachet7))]
+
+
+print(alegere_optional6())
