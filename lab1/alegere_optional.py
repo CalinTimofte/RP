4fc8f20baea9fa37fc74469cd8c2e3bc2fcf85c1
unknown <calin.timofte@yahoo.com>
2020-03-03 10:54:09 +0200

    Optionale pachet5.

--- lab1/alegere_optional.py
+++ lab1/alegere_optional.py
@@ -16,6 +16,37 @@ intrebari_pachet_7 = ["Este interesat de Retele Petri?",
                       "Vrei sa stii cum functioneaza cardul pe care iti tii banii?",
                       "Vrei sa inveti .NET?"]
 
+raspunsuri = ["Da", "Nu stiu", "Nu"]
+intrebari_pachet_5 = ["Iti place programarea functionala?", "Esti interesat de programarea Android?",
+                      "Iti place partea matematica a informaticii?"]
+
+def alegere_optional_pachet5():
+    print("Intrebari pt alegerea optionalului din pachet5:")
+    raspunsuri_utilizator = []
+    intrare_user = None
+    for i, j in enumerate(intrebari_pachet_5):
+        print(j)
+        for x, y in enumerate(raspunsuri):
+            print(x, y)
+        while intrare_user not in range(len(raspunsuri)):
+            print("introduceti: ", end="")
+            for elem in range(len(raspunsuri) - 1):
+                print(str(elem) + " ", end="")
+            print("sau " + str(len(raspunsuri) - 1))
+            try:
+                intrare_user = int(input())
+            except:
+                print("Input incorect")
+        raspunsuri_utilizator.append(intrare_user)
+        intrare_user = None
+    try:
+        x = raspunsuri_utilizator.index(0)
+    except:
+        try:
+            x = raspunsuri_utilizator.index(1)
+        except:
+            x = 0
+    print("Optionalul ales din pachetul 5 este: " + pachet5[x])
 
 def alegere_optional6():
     print("Alegere optional pachet 6")
@@ -43,4 +74,8 @@ def alegere_optional7():
     return pachet7[raspunsuri_pachet7.index(max(raspunsuri_pachet7))]
 
 
-print(alegere_optional6())
+alegere_optional_pachet5()
+print()
+print("Optionalul ales din pachetul 6 este: " + alegere_optional6())
+print()
+print("Optionalul ales din pachetul 7 este: " + alegere_optional7())
