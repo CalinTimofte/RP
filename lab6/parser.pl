edce24426dce44b4d368e1161940991e726e7d8e
platicaalex <platicaalex@gmail.com>
2020-03-31 19:12:54 +0300

    Maybe update

--- lab6/parser.pl
+++ lab6/parser.pl
@@ -36,7 +36,7 @@
 s(In,Out):-np(In,In2),np(In2,In3),vp(In3,Out).
 s(In,Out):-vp(In,In2),np(In2,Out).
 vp(In,Out):-det(In,In2),v(In2,Out).
-np(In,Out):-det(In,In2), n(In2,Out).
+np(In,Out):-det(In,In2),n(In2,Out).
 
 det([o|W],W).
 det([am|W],W).
