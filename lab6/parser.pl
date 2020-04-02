ad8131ff3dc3e7cd2c7b0ed2229c7b2f61094e22
platicaalex <platicaalex@gmail.com>
2020-04-02 17:49:50 +0300

    Update parser.pl

--- lab6/parser.pl
+++ lab6/parser.pl
@@ -1,38 +1,5 @@
 % Timofte Călin, Plătică Alexandru-Gabriel
 
-% S->AS'
-% S'->BC
-% S'->DE
-% S->ECAB
-% C->C1 C2
-% E->E1 E2
-% A->am
-% B->citit
-% C1->o 
-% C2->carte
-% D->mers
-% E1->la
-% E2->facultate
-
-% VARIANTA 1
-% s(In,Out):-a(In,In2), sprim(In2,Out).
-% sprim(In,Out):-b(In,In2), c(In2,Out).
-% sprim(In,Out):-d(In,In2), e(In2,Out).
-% s(In,Out):-e(In,In2),c(In3,In4),a(In4,In5),b(In5,Out).
-% c(In,Out):-c1(In,In2),c2(In2,Out).
-% e(In,Out):-e1(In,In2),e2(In2,Out).
-
-% a([am|W],W).
-% b([citit|W],W).
-% c1([o|W],W).
-% c2([carte|W],W).
-% d([mers|W],W).
-% e1([la|W],W).
-% e2([facultate|W],W).
-
-
-% VARIANTA 2
-% to do: counter cu numarul de aplicari de reguli
 s(In,Out):-np(In,In2),np(In2,In3),vp(In3,Out).
 s(In,Out):-vp(In,In2),np(In2,Out).
 vp(In,Out):-det(In,In2),v(In2,Out).
